62dac86221915bd450eb5fb5a649cf6dcc243c99
shahpay <shahpay@amazon.co.uk>
2023-07-14 15:19:13 +0100

    modified the values

--- terraform/controlstest.tf
+++ terraform/controlstest.tf
@@ -39,6 +39,8 @@ module "controls" {
     budget = "600"
     group1 = "testgroup1"
     group2 = "testgroup2"
+    type = "Core"
+    environment = "Prod"
   }
 
   account_customizations_name = "dtpl-dev"
